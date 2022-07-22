c8457d5f3a0b8040ce6e564fdea85591f90c5800
BhaveshShahani <gudd.shahani@gmail.com>
2022-07-23 04:52:30 +0530

    side nav foot

---
+++ footer/foot.js
@@ -0,0 +1,72 @@
+let foot= ()=>{
+    return `<div id="decoration"></div>
+    <div id="links">
+        <div>
+            <p>Get email offers & the latest news from Bath & Body Works!</p>
+            <p>Enter Email</p>
+            <input type="text" id="email" placeholder="Email">
+            <p>Confirm Email</p>
+            <input type="text" id="reEmail" placeholder="Re-Enter Email">
+            <input type="Submit" id="submit">
+        </div>
+        <div>
+            <h3>Coustmer Care</h3>
+            <p><a href="">Help & FAQs</a></p>
+            <p><a href="">Shipping</a></p>
+            <p><a href="">Returns & Exchanges</a></p>
+            <p><a href="">Order Tracking</a></p>
+            <p><a href="">Corporate Sales & Gifts</a></p>
+            <p><a href="">Contact Us</a></p>
+        </div>
+        <div>
+            <h3>My Account</h3>
+            <p><a href="">Sign In or Sign Up</a></p>
+            <p><a href="">Order Tracking</a></p>
+            <p><a href="">My Auto Refresh</a></p>
+            <p><a href="">My Love-It List</a></p>
+        </div>
+        <div>
+            <h3>Discover</h3>
+            <p><a href="">About Us</a></p>
+            <p><a href="">Careers</a></p>
+            <p><a href="">Gift Cards</a></p>
+            <p><a href="">Shop by Fragrance</a></p>
+            <p><a href="">Product Ingredients</a></p>
+            <p><a href="">Get Inspired</a></p>
+            <p><a href="">Diversity, Equity & Inclusion</a></p>
+        </div>
+        <div>
+            <h3>Find Us</h3>
+            <p><a href="">Store Location</a></p>
+            <p><a href="">Global Locations</a></p>
+        </div>
+    </div>
+    <div id="connect">
+        <div id="social">
+            <h3>Get Connected</h3>
+            <img src="https://icons-for-free.com/download-icon-facebook+logo+media+network+social+icon-1320192913497992427_512.png" alt="Facebook">
+            <img src="https://static.vecteezy.com/system/resources/previews/002/534/045/original/social-media-twitter-logo-blue-isolated-free-vector.jpg" alt="twitter">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png" alt="Insta">
+            <img src="https://static.vecteezy.com/system/resources/previews/006/693/634/original/tik-tok-flat-icon-template-black-color-editable-tik-tok-flat-icon-symbol-flat-illustration-for-graphic-and-web-design-free-vector.jpg" alt="tictok">
+            <img src="https://centa.org/wp-content/uploads/2021/03/YouTube-logo.jpg" alt="youtube">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Pinterest-logo.png" alt="pintrest">
+        </div>
+        <div id="logo">
+            <img src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw0c8e6af7/images/evergreen/Happiness_Guaranteedtimes2_v2.jpg?yocs=o_s_" alt="logo">
+        </div>
+    </div>
+    <div id="tc">
+        <div id="privacy">
+            <p>Terms Of Use</p>
+            <p>Privacy Policy</p>
+            <p>Security Bug Report</p>
+            <p>California Privacy Rights</p>
+            <p>Do Not Sell My Personal Information (California)</p>
+            <p>Transparency in Supply Chains</p>
+            <p>Ad Preference</p>
+        </div>
+        <div id="rights"><p>&#169; Bath & Body Works Direct, Inc. All Right Reserved</p></div>
+    </div>`
+}
+
+export default foot;
