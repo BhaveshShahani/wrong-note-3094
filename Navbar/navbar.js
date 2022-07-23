3c4fb096665176209271ac4bbc4fbfdc037e8411
BhaveshShahani <gudd.shahani@gmail.com>
2022-07-23 18:51:52 +0530

    change

--- Navbar/navbar.js
+++ Navbar/navbar.js
@@ -1,221 +1,223 @@
 let navbar = ()=>{
  return `
  <!-- Promo Div start -->
- <div id="promo">
-     <img src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/on/demandware.static/Sites-BathAndBodyWorks-Site/-/en_US/v1658310375937/images/svg-icons/bopis-icon-small.svg?yocs=o_s_" alt="promo">
-     <h5>Pick up in store</h5>
-     <p>Set Store</p>
- </div>
- <!-- Promo Div end -->
- <!-- Logo and search bar div start -->
- <div id="wrap">
-     <img id="logo" src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/on/demandware.static/Sites-BathAndBodyWorks-Site/-/default/dwe188b42c/images/svg-icons/Logos-main.svg?yocs=o_s_" alt="logo">
-     <div id="section">
-         <input type="search" id="search" placeholder="Search by fragrance or product..." >
-         <img id="input_img" src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/on/demandware.static/Sites-BathAndBodyWorks-Site/-/default/dw517a8476/images/svg-icons/UI-Search-v2.svg?yocs=o_s_" alt="">
-         <img src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/on/demandware.static/Sites-BathAndBodyWorks-Site/-/default/dw0d2d8374/images/svg-icons/UI-MyAccount.svg?yocs=o_s_" alt="LogIn">
-         <img src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/on/demandware.static/Sites-BathAndBodyWorks-Site/-/en_US/v1658310375937/images/svg-icons/UI-AddToBag.svg?yocs=o_s_" alt="Cart Display">
-     </div>  
- </div>
- <!-- Logo and search bar div end -->
- <!-- Navigator fields start -->
- <div id="menu">
-     <ul>
-      <li class="line"><a href="#">Top Offer</a></li>
-      <!-- Menu -->
-      <li class="line"><a href="#">Body Care</a>
-          <!-- Sub Menu -->
-          <ul>
-              <li><a href="#">Bath & Shower &#10095;</a>
-                  <!-- Deep Sub Menu -->
-                  <ul>
-                      <li><a href="#">All Bath & Shower</a></li>
-                      <li><a href="#">Body Wash & Shower Gel</a></li>
-                      <li><a href="#">Bubble Bath</a></li>
-                      <li><a href="#">Body Scrub</a></li>
-                      <li><a href="#">Bar Soap</a></li>
-                      <li><a href="#">Bath Accessories</a></li>
-                  </ul>
-              </li>
-              <li><a href="#">Moisturizers &#10095;</a>
-                  <ul>
-                      <li><a href="#">All Moisturizers</a></li>
-                      <li><a href="#">Body Lotion</a></li>
-                      <li><a href="#">Body Cream</a></li>
-                      <li><a href="#">Hand Cream</a></li>
-                      <li><a href="#">Lip Gloss & Balms</a></li>
-                  </ul>
-              </li>
-              <li><a href="#">Fragrance &#10095;</a>
-                 <ul>
-                     <li><a href="#">All Fragrance</a></li>
-                     <li><a href="#">Body Sprays & Mists</a></li>
-                     <li><a href="#">Perfume & Cologne</a></li>
-                 </ul>
-             </li>
-              <li><a href="#">Aromatherapy &#10095;</a>
-                 <ul>
-                     <li><a href="#">All Aromatherapy</a></li>
-                     <li><a href="#">Body Wash & Shower Gel</a></li>
-                     <li><a href="#">Moisturizers</a></li>
-                     <li><a href="#">Mists & Sprays</a></li>
-                 </ul>
-             </li>
-              <li><a href="#">Features &#10095;</a>
-                 <ul>
-                     <li><a href="#">Retired Fragrance</a></li>
-                     <li><a href="#">Travel Size</a></li>
-                 </ul>
-             </li>
-              <li><a href="#">Explore Body Care </a></li>
-          </ul>
-      </li>
-      <li class="line"><a href="#">Candles</a>
-         <ul>
-             <li><a href="#">All Candles &#10095;</a>
-                 <!-- Deep Sub Menu -->
-                 <ul>
-                     <li><a href="#">3-Wick Candles</a></li>
-                     <li><a href="#">Single Wick Candles</a></li>
-                     <li><a href="#">Candle Holders & Accessories</a></li>
-                 </ul>
-             </li>
-             <li><a href="#">Featured &#10095;</a>
-                 <ul>
-                     <li><a href="#">The White Barn Shop</a></li>
-                 </ul>
-             </li>
-             <li><a href="#">Explore Candles</a>
-            </li>
-         </ul>
-     </li>
-      <li class="line"><a href="#">Home Fragrance</a>
-         <ul>
-             <li><a href="#">Air Fresheners &#10095;</a>
-                 <!-- Deep Sub Menu -->
-                 <ul>
-                     <li><a href="#">All Wallflowers</a></li>
-                     <li><a href="#">Wallflowers Plugs</a></li>
-                     <li><a href="#">Room Sprays & Mists </a></li>
-                     <li><a href="#">Car Fragrance</a></li>
-                 </ul>
-             </li>
-             <li><a href="#">Featured &#10095;</a>
-                 <ul>
-                     <li><a href="#">All Refresh Wallflowers</a></li>
-                 </ul>
-             </li>
-             <li><a href="#">Explore Home Fragrance</a>
-            </li>
-         </ul>
-     </li>
-      <li class="line"><a href="#">Hand Soaps & Sanitizers</a>
-         <ul>
-             <li><a href="#">Hand Soaps &#10095;</a>
-                 <!-- Deep Sub Menu -->
-                 <ul>
-                     <li><a href="#">All Hand Soaps</a></li>
-                     <li><a href="#">Foaming Soaps</a></li>
-                     <li><a href="#">Gel Soaps</a></li>
-                     <li><a href="#">Soap Holders</a></li>
-                 </ul>
-             </li>
-             <li><a href="#">Hand Sanitizers &#10095;</a>
-                 <ul>
-                     <li><a href="#">All Hand Sanitizers</a></li>
-                     <li><a href="#">PocketBac Hand Sanitizers</a></li>
-                     <li><a href="#">Hand Sanitizer Sprays</a></li>
-                     <li><a href="#">Full Size Sanitizers</a></li>
-                     <li><a href="#">Hand Sanitizer Holders</a></li>
-                 </ul>
-             </li>
-             <li><a href="#">Featured &#10095;</a>
-                <ul>
-                    <li><a href="#">Gentle & Clean Hand Soaps</a></li>
-                </ul>
-            </li>
-             <li><a href="#">Explore Hand Soaps & Sanitizer</a>
-            </li>
-         </ul>
-      </li>
-      <li class="line"><a href="#">Mens's</a>
-         <ul>
-             <li><a href="#">The Men's Shop &#10095;</a>
-                 <!-- Deep Sub Menu -->
-                 <ul>
-                     <li><a href="#">All Men's Body Care</a></li>
-                     <li><a href="#">Body Wash & Shower Gel</a></li>
-                     <li><a href="#">Moisturizers</a></li>
-                     <li><a href="#">Body Sprays & Cologne</a></li>
-                     <li><a href="#">Home Fragrance</a></li>
-                 </ul>
-             </li>
-             <li><a href="#">Explore Men's</a>
-             </li>
-         </ul>
-     </li>
-      <li class="line"><a href="#">Gifts</a>
-         <ul>
-             <li><a href="#">Featured &#10095;</a>
-                 <!-- Deep Sub Menu -->
-                 <ul>
-                     <li><a href="#">Gift Set</a></li>
-                 </ul>
-             </li>
-             <li><a href="#">Gifts By Price &#10095;</a>
-                 <ul>
-                     <li><a href="#">$15 & Under Gifts</a></li>
-                     <li><a href="#">$30 & Under Gifts</a></li>
-                     <li><a href="#">$50 & Under Gifts</a></li>
-                 </ul>
-             </li>
-             <li><a href="#">Gift By Recipent &#10095;</a>
-                <ul>
-                    <li><a href="#">Gifts For Her</a></li>
-                    <li><a href="#">Gifts For Him</a></li>
-                    <li><a href="#">Gifts For Everyone</a></li>
-                    <li><a href="">Gifts For the Hostess</a></li>
-                </ul>
-            </li>
-             <li><a href="#">Collections &#10095;</a>
-                <ul>
-                    <li><a href="#">Gift Boxes & Bags</a></li>
-                    <li><a href="#">Accessories</a></li>
+    <div id="promo">
+        <img src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/on/demandware.static/Sites-BathAndBodyWorks-Site/-/en_US/v1658310375937/images/svg-icons/bopis-icon-small.svg?yocs=o_s_" alt="promo">
+        <h5>Pick up in store</h5>
+        <p>Set Store</p>
+    </div>
+    <!-- Promo Div end -->
+    <!-- Logo and search bar div start -->
+    <div id="wrap">
+        <a id="logo"  href="../home-page/home.html"><img src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/on/demandware.static/Sites-BathAndBodyWorks-Site/-/default/dwe188b42c/images/svg-icons/Logos-main.svg?yocs=o_s_" alt="logo"></a>
+        <div id="section">
+            <input type="search" id="search" placeholder="Search by fragrance or product..." >
+            <img id="input_img" src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/on/demandware.static/Sites-BathAndBodyWorks-Site/-/default/dw517a8476/images/svg-icons/UI-Search-v2.svg?yocs=o_s_" alt="">
+            <a id="g" href="../login/login.html"><img src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/on/demandware.static/Sites-BathAndBodyWorks-Site/-/default/dw0d2d8374/images/svg-icons/UI-MyAccount.svg?yocs=o_s_" alt="LogIn"></a>
+            <img src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/on/demandware.static/Sites-BathAndBodyWorks-Site/-/en_US/v1658310375937/images/svg-icons/UI-AddToBag.svg?yocs=o_s_" alt="Cart Display">
+        </div>  
+    </div>
+    <!-- Logo and search bar div end -->
+    <!-- Navigator fields start -->
+
+    <div id="menu">
+        <ul>
+         <li class="line"><a href="#">Top Offer</a></li>
+         <!-- Menu -->
+         <li class="line"><a href="../productpage/bodycare.html">Body Care</a>
+             <!-- Sub Menu -->
+             <ul>
+                 <li><a href="#">Bath & Shower &#10095;</a>
+                     <!-- Deep Sub Menu -->
+                     <ul>
+                         <li><a href="#">All Bath & Shower</a></li>
+                         <li><a href="#">Body Wash & Shower Gel</a></li>
+                         <li><a href="#">Bubble Bath</a></li>
+                         <li><a href="#">Body Scrub</a></li>
+                         <li><a href="#">Bar Soap</a></li>
+                         <li><a href="#">Bath Accessories</a></li>
+                     </ul>
+                 </li>
+                 <li><a href="#">Moisturizers &#10095;</a>
+                     <ul>
+                         <li><a href="#">All Moisturizers</a></li>
+                         <li><a href="#">Body Lotion</a></li>
+                         <li><a href="#">Body Cream</a></li>
+                         <li><a href="#">Hand Cream</a></li>
+                         <li><a href="#">Lip Gloss & Balms</a></li>
+                     </ul>
+                 </li>
+                 <li><a href="#">Fragrance &#10095;</a>
+                    <ul>
+                        <li><a href="#">All Fragrance</a></li>
+                        <li><a href="#">Body Sprays & Mists</a></li>
+                        <li><a href="#">Perfume & Cologne</a></li>
                     </ul>
-            </li>
-             <li><a href="#">Gift Cards &#10095;</a>
-                <ul>
-                    <li><a href="#">E-Gift Cards</a></li>
-                    <li><a href="#">Gift Cards</a></li>
-                </ul>
-            </li>
-             <li><a href="#">Explore Gifts </a></li>
-         </ul>
-     </li>
-      <li class="line"><a href="#">New & Now</a>
-         <!-- <ul>
-             <li><a href="#">New & Now &#10095;</a>
-                 Deep Sub Menu
-                 <ul>
-                     <li><a href="#">New Arrivals</a></li>
-                     <li><a href="#">Aromatherapy Shop</a></li>
-                     <li><a href="#">Fall Collection</a></li>
-                     <li><a href="#">Halloween</a></li>
-                     <li><a href="#">Best Sellers</a></li>
-                 </ul>
-             </li>
-             <li><a href="#">Explore New & Now</a>
-             </li>
+                </li>
+                 <li><a href="#">Aromatherapy &#10095;</a>
+                    <ul>
+                        <li><a href="#">All Aromatherapy</a></li>
+                        <li><a href="#">Body Wash & Shower Gel</a></li>
+                        <li><a href="#">Moisturizers</a></li>
+                        <li><a href="#">Mists & Sprays</a></li>
+                    </ul>
+                </li>
+                 <li><a href="#">Features &#10095;</a>
+                    <ul>
+                        <li><a href="#">Retired Fragrance</a></li>
+                        <li><a href="#">Travel Size</a></li>
+                    </ul>
+                </li>
+                 <li><a href="#">Explore Body Care </a></li>
+             </ul>
+         </li>
+         <li class="line"><a href="../productpage/product2.html">Candles</a>
+            <ul>
+                <li><a href="#">All Candles &#10095;</a>
+                    <!-- Deep Sub Menu -->
+                    <ul>
+                        <li><a href="#">3-Wick Candles</a></li>
+                        <li><a href="#">Single Wick Candles</a></li>
+                        <li><a href="#">Candle Holders & Accessories</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Featured &#10095;</a>
+                    <ul>
+                        <li><a href="#">The White Barn Shop</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Explore Candles</a>
+               </li>
+            </ul>
+        </li>
+         <li class="line"><a href="../productpage/product3.html">Home Fragrance</a>
+            <ul>
+                <li><a href="#">Air Fresheners &#10095;</a>
+                    <!-- Deep Sub Menu -->
+                    <ul>
+                        <li><a href="#">All Wallflowers</a></li>
+                        <li><a href="#">Wallflowers Plugs</a></li>
+                        <li><a href="#">Room Sprays & Mists </a></li>
+                        <li><a href="#">Car Fragrance</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Featured &#10095;</a>
+                    <ul>
+                        <li><a href="#">All Refresh Wallflowers</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Explore Home Fragrance</a>
+               </li>
+            </ul>
+        </li>
+         <li class="line"><a href="../productpage/product4.html">Hand Soaps & Sanitizers</a>
+            <ul>
+                <li><a href="#">Hand Soaps &#10095;</a>
+                    <!-- Deep Sub Menu -->
+                    <ul>
+                        <li><a href="#">All Hand Soaps</a></li>
+                        <li><a href="#">Foaming Soaps</a></li>
+                        <li><a href="#">Gel Soaps</a></li>
+                        <li><a href="#">Soap Holders</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Hand Sanitizers &#10095;</a>
+                    <ul>
+                        <li><a href="#">All Hand Sanitizers</a></li>
+                        <li><a href="#">PocketBac Hand Sanitizers</a></li>
+                        <li><a href="#">Hand Sanitizer Sprays</a></li>
+                        <li><a href="#">Full Size Sanitizers</a></li>
+                        <li><a href="#">Hand Sanitizer Holders</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Featured &#10095;</a>
+                   <ul>
+                       <li><a href="#">Gentle & Clean Hand Soaps</a></li>
+                   </ul>
+               </li>
+                <li><a href="#">Explore Hand Soaps & Sanitizer</a>
+               </li>
+            </ul>
+         </li>
+         <li class="line"><a href="../productpage/product5.html">Mens's</a>
+            <ul>
+                <li><a href="#">The Men's Shop &#10095;</a>
+                    <!-- Deep Sub Menu -->
+                    <ul>
+                        <li><a href="#">All Men's Body Care</a></li>
+                        <li><a href="#">Body Wash & Shower Gel</a></li>
+                        <li><a href="#">Moisturizers</a></li>
+                        <li><a href="#">Body Sprays & Cologne</a></li>
+                        <li><a href="#">Home Fragrance</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Explore Men's</a>
+                </li>
+            </ul>
+        </li>
+         <li class="line"><a href="#">Gifts</a>
+            <ul>
+                <li><a href="#">Featured &#10095;</a>
+                    <!-- Deep Sub Menu -->
+                    <ul>
+                        <li><a href="#">Gift Set</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Gifts By Price &#10095;</a>
+                    <ul>
+                        <li><a href="#">$15 & Under Gifts</a></li>
+                        <li><a href="#">$30 & Under Gifts</a></li>
+                        <li><a href="#">$50 & Under Gifts</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Gift By Recipent &#10095;</a>
+                   <ul>
+                       <li><a href="#">Gifts For Her</a></li>
+                       <li><a href="#">Gifts For Him</a></li>
+                       <li><a href="#">Gifts For Everyone</a></li>
+                       <li><a href="">Gifts For the Hostess</a></li>
+                   </ul>
+               </li>
+                <li><a href="#">Collections &#10095;</a>
+                   <ul>
+                       <li><a href="#">Gift Boxes & Bags</a></li>
+                       <li><a href="#">Accessories</a></li>
+                       </ul>
+               </li>
+                <li><a href="#">Gift Cards &#10095;</a>
+                   <ul>
+                       <li><a href="#">E-Gift Cards</a></li>
+                       <li><a href="#">Gift Cards</a></li>
+                   </ul>
+               </li>
+                <li><a href="#">Explore Gifts </a></li>
+            </ul>
+        </li>
+         <li class="line"><a href="#">New & Now</a>
+            <!-- <ul>
+                <li><a href="#">New & Now &#10095;</a>
+                    Deep Sub Menu
+                    <ul>
+                        <li><a href="#">New Arrivals</a></li>
+                        <li><a href="#">Aromatherapy Shop</a></li>
+                        <li><a href="#">Fall Collection</a></li>
+                        <li><a href="#">Halloween</a></li>
+                        <li><a href="#">Best Sellers</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Explore New & Now</a>
+                </li>
 
-         </ul> -->
-     </li>
-     </ul>
- </div>
+            </ul> -->
+        </li>
+        </ul>
 
- <!-- Navigator fields end -->
- <!-- Decoration Line start -->
- <div style=" border-bottom: solid #8080804b;"></div>
-  <!-- Decoration Line ends -->
+    </div>
+   
+    <!-- Navigator fields end -->
+    <!-- Decoration Line start -->
+    <div style=" border-bottom: solid #8080804b;"></div>
+     <!-- Decoration Line ends -->
 `
 }
 
